77f1ba1a389759db653fe6d498692f1342bc0985
Fernando Rocha <fernandorsl@gmail.com>
2016-09-11 22:57:59 -0500

    Including dependencies from carthage and pods. Facebook - Carthage Firebase - Pods

--- GoodeChallenge/AppDelegate.swift
+++ GoodeChallenge/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import Firebase
+import FacebookLogin
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        
+       // return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
@@ -39,8 +46,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
     }
-
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool
+    {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
 }
 
--- GoodeChallenge/ViewController.swift
+++ GoodeChallenge/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import FacebookLogin
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = LoginButton(readPermissions: [ .PublicProfile, .Email, .UserFriends ])
+        
+        loginButton.delegate = self
+        loginButton.center = self.view.center;
+        
+        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func loginButtonDidCompleteLogin(loginButton: LoginButton, result: LoginResult) {
+        switch result {
+            case .Failed(let error):
+                print(error)
+            case .Cancelled:
+                print("User cancelled login.")
+            case .Success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: LoginButton) {
+        print("User Logged Out")
+    }
 
 }
 
